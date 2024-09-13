7fa2f91c5aea54dd950cef1c85318a3b15d9985a
Artur Trombim <114882647+Tuizim@users.noreply.github.com>
2024-09-12 22:25:18 -0300

    [ADD] Logo images

--- commom/componentsjs.js
+++ commom/componentsjs.js
@@ -56,7 +56,7 @@ function PageHeader() {
     <nav class="navbar navbar-expand-lg bg-body-tertiary shadow">
         <div class="container-fluid">
           <span class="navbar-brand no-select" >
-          ${LOGOIMAGE}
+          ${LOGOIMAGE} Ohana
           </span>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
